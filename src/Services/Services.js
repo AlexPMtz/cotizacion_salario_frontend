c1d1fe0332b4d130ca535c7ac7fd855cc5ab9010
alexisyair.patino.contractor <alexisyair.patino.contractor@bbva.com>
2023-05-15 11:38:38 -0600

    [Add](Services) Modificación en la ruta de la API, le pasamos la ruda de la API desplegada

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,7 +1,6 @@
 import Axios from "axios";
 
 const createCreditQuote = (normalRate, punctualRate, productId, price, weeks) => {
-  console.log('----->HOLA');
   let body = {
     normalRate,
     punctualRate,
@@ -9,22 +8,22 @@ const createCreditQuote = (normalRate, punctualRate, productId, price, weeks) =>
     price,
     weeks
   }
-  return Axios.post("http://localhost:3000/creditQuotes", {...body});
+  return Axios.post("http://ec2-44-202-114-168.compute-1.amazonaws.com:3000/creditQuotes", {...body});
 }
 
 const getProducts = () => {
-  return Axios.get("http://localhost:3000/products");
+  return Axios.get("http://ec2-44-202-114-168.compute-1.amazonaws.com:3000/products");
 }
 
 const createDeadline = (weeks) => {
   let body = {
     weeks
   }
-  return Axios.post("http://localhost:3000/deadlines", {...body});
+  return Axios.post("http://ec2-44-202-114-168.compute-1.amazonaws.com:3000/deadlines", {...body});
 }
 
 const getDeadlines = () => {
-  return Axios.get("http://localhost:3000/deadlines");
+  return Axios.get("http://ec2-44-202-114-168.compute-1.amazonaws.com:3000/deadlines");
 }
 
 export {
