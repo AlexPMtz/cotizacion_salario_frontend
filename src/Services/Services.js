a7078edf57f2adc0c4cc3d324bdfa72ad6236804
alexisyair.patino.contractor <alexisyair.patino.contractor@bbva.com>
2023-05-15 12:36:11 -0600

    prueba https

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -8,22 +8,22 @@ const createCreditQuote = (normalRate, punctualRate, productId, price, weeks) =>
     price,
     weeks
   }
-  return Axios.post("https://ec2-44-202-114-168.compute-1.amazonaws.com:3000/creditQuotes", {...body});
+  return Axios.post("http://ec2-44-202-114-168.compute-1.amazonaws.com:3000/creditQuotes", {...body});
 }
 
 const getProducts = () => {
-  return Axios.get("https://ec2-44-202-114-168.compute-1.amazonaws.com:3000/products");
+  return Axios.get("http://ec2-44-202-114-168.compute-1.amazonaws.com:3000/products");
 }
 
 const createDeadline = (weeks) => {
   let body = {
     weeks
   }
-  return Axios.post("https://ec2-44-202-114-168.compute-1.amazonaws.com:3000/deadlines", {...body});
+  return Axios.post("http://ec2-44-202-114-168.compute-1.amazonaws.com:3000/deadlines", {...body});
 }
 
 const getDeadlines = () => {
-  return Axios.get("https://ec2-44-202-114-168.compute-1.amazonaws.com:3000/deadlines");
+  return Axios.get("http://ec2-44-202-114-168.compute-1.amazonaws.com:3000/deadlines");
 }
 
 export {
